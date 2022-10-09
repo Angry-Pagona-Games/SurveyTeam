54a437dd72685acb9e16c6a8859c176edfed89fe
Angry <sharrollsmith@angrypagonagames.com>
2022-10-09 09:32:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SurveyTeam
+
+Developed with Unreal Engine 5
---
+++ Source/SurveyTeam/SurveyTeamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurveyTeamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURVEYTEAM_API ASurveyTeamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
